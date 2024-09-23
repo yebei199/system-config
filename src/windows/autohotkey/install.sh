49e74d74c4b7d9376ddd0144e6b98f6bcdac08a8
yebei199 <scauyb@qq.com>
2024-09-23 15:56:56 +0800

    累了
    Signed-off-by: yebei199 <scauyb@qq.com>

--- src/windows/autohotkey/install.sh
+++ src/windows/autohotkey/install.sh
@@ -1,1 +1,2 @@
-cp "./scan1.ahk" "~/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/"
+scoop install autohotkey
+#cp "./scan1.ahk" "~/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/"
